ac3ff3be04c013130d733ead8d197778d21a2d24
JamesBond777 <41854763+JamesBond777@users.noreply.github.com>
2018-07-29 18:42:36 +0530

    Add files via upload

--- scripts/config_xml.js
+++ scripts/config_xml.js
@@ -2,8 +2,8 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description tsc:version="2.0.1" dc:date="2018-07-29 03:23:40 PM" dc:source="Camtasia Studio,8.1.2,enu" dc:title="Untitled4" tscDM:firstFrame="Untitled4_First_Frame.png" tscDM:originId="979B1895-8A25-4F66-9167-5A50FB6899D0" tscDM:project="Untitled3">\
-         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="11800"/>\
+      <rdf:Description tsc:version="2.0.1" dc:date="2018-07-29 06:38:51 PM" dc:source="Camtasia Studio,8.1.2,enu" dc:title="EngagementInvitation" tscDM:firstFrame="EngagementInvitation_First_Frame.png" tscDM:originId="979B1895-8A25-4F66-9167-5A50FB6899D0" tscDM:project="Untitled3">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="29733"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="480" stDim:w="854"/>\
          <tsc:langName>\
             <rdf:Bag>\
@@ -15,7 +15,7 @@ TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
                   <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
                      <xmpDM:markers>\
                         <rdf:Seq>\
-                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="6000" xmpDM:duration="5000" tscDM:boundingPoly="356,87;479,87;479,332;356,332;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://goo.gl/maps/GpsnWaBwb8J2" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
+                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="15370" xmpDM:duration="12260" tscDM:boundingPoly="0,3;847,3;847,479;0,479;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://goo.gl/maps/GpsnWaBwb8J2" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
                      </xmpDM:markers>\
                   </rdf:Description>\
                </rdf:li>\
@@ -37,7 +37,7 @@ TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
             <rdf:Description>\
                <tscDM:files>\
                   <rdf:Seq>\
-                     <rdf:li xmpDM:name="0" xmpDM:value="Untitled4.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Untitled4_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="Untitled4_Thumbnails.png"/></rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="EngagementInvitation.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="EngagementInvitation_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="EngagementInvitation_Thumbnails.png"/></rdf:Seq>\
                </tscDM:files>\
             </rdf:Description>\
          </tscDM:contentList>\
